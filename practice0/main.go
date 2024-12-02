20f9eeb2fa300d5b0ba9bc562616b805f0d5d1e0
ksenia274 <kcen,yackubowa@2013@gmail.com>
2024-12-02 23:36:02 +0300

    first practice is ready

--- practice0/main.go
+++ practice0/main.go
@@ -1,8 +1,51 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
 func main() {
+	b := make([]byte, 100)
+
+	// Первая горутина
+	go func() {
+		for {
+			filler(b[:50], '0', '1')
+			time.Sleep(time.Second)
+		}
+	}()
+
+	// Вторая горутина
+	go func() {
+		for {
+			filler(b[50:], 'X', 'Y')
+			time.Sleep(time.Second)
+		}
+	}()
+
+	// Третья горутина
+	go func() {
+		for {
+			fmt.Println(string(b))
+			time.Sleep(time.Second)
+		}
+	}()
+
+	// Основной цикл
+	for {
+	}
 }
 
-func filler(b []byte, ifzero byte, ifnot byte){
-
+func filler(b []byte, ifzero byte, ifnot byte) {
+	rand.Seed(time.Now().UnixNano())
+	for i := range b {
+		r := rand.Intn(2)
+		if r == 0 {
+			b[i] = ifzero
+		} else {
+			b[i] = ifnot
+		}
+	}
 }
--- practice0/main_test.go
+++ practice0/main_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"slices"
 	"testing"
 )
 
@@ -10,5 +9,24 @@ func TestFiller(t *testing.T) {
 	zero := byte('0')
 	one := byte('1')
 	filler(b[:], zero, one)
-	
+
+	foundZero := false
+	foundOne := false
+
+	for _, v := range b {
+		if v == zero {
+			foundZero = true
+		}
+		if v == one {
+			foundOne = true
+		}
+	}
+
+	if !foundZero {
+		t.Errorf("Expected to find '%c' in the slice, but it was not found.", zero)
+	}
+
+	if !foundOne {
+		t.Errorf("Expected to find '%c' in the slice, but it was not found.", one)
+	}
 }
